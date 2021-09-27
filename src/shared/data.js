3799f67d47a04ad38c903be32b22cdfb1c0a6fd7
Abdul Hanan Asif <59803552+abdulhanan5643@users.noreply.github.com>
2021-09-27 08:41:40 +0500

    data created

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,12 +1,525 @@
 export const PRODUCTS = [
     {
         id: 0,
-        name: 'Peter Pan',
-        image: '/assets/images/alberto.png',
-        category: 'Chief Epicurious Officer',
-        new:true,
-        ranking:'top',
+        name: 'Product',
+        image: require('../../shared/images/sweater/sweater1.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'top',
         featured: false,
-        description: "Our CEO, Peter, credits his hardworking East Asian immigrant parents who undertook the arduous journey to the shores of America with the intention of giving their children the best future. His mother's wizardy in the kitchen whipping up the tastiest dishes with whatever is available inexpensively at the supermarket, was his first inspiration to create the fusion cuisines for which The Frying Pan became well known. He brings his zeal for fusion cuisines to this restaurant, pioneering cross-cultural culinary connections."
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 1,
+        name: 'Product',
+        image: require('../../shared/images/sweater/sweater2.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'top',
+        featured: true,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 2,
+        name: 'Product',
+        image: require('../../shared/images/sweater/sweater3.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'middle',
+        featured: false,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 3,
+        name: 'Product',
+        image: require('../../shared/images/sweater/sweater4.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'last',
+        featured: true,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 4,
+        name: 'Product',
+        image: require('../../shared/images/sweater/sweater5.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'last',
+        featured: false,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 5,
+        name: 'Product',
+        image: require('../../shared/images/sweater/sweater6.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'middle',
+        featured: false,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 6,
+        name: 'Product',
+        image: require('../../shared/images/suit/suit1.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'middle',
+        featured: false,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 7,
+        name: 'Product',
+        image: require('../../shared/images/suit/suit2.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'last',
+        featured: false,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 8,
+        name: 'Product',
+        image: require('../../shared/images/suit/suit3.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'last',
+        featured: true,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 9,
+        name: 'Product',
+        image: require('../../shared/images/suit/suit4.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'middle',
+        featured: false,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 10,
+        name: 'Product',
+        image: require('../../shared/images/suit/suit5.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'top',
+        featured: false,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 11,
+        name: 'Product',
+        image: require('../../shared/images/hoodie/hoodie1.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'last',
+        featured: true,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 12,
+        name: 'Product',
+        image: require('../../shared/images/hoodie/hoodie2.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'last',
+        featured: true,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 13,
+        name: 'Product',
+        image: require('../../shared/images/hoodie/hoodie3.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'middle',
+        featured: false,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 14,
+        name: 'Product',
+        image: require('../../shared/images/hoodie/hoodie4.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'middle',
+        featured: false,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 15,
+        name: 'Product',
+        image: require('../../shared/images/shirt/shirt1.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'middle',
+        featured: false,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 16,
+        name: 'Product',
+        image: require('../../shared/images/shirt/shirt2.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'last',
+        featured: true,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 17,
+        name: 'Product',
+        image: require('../../shared/images/shirt/shirt3.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'top',
+        featured: false,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 18,
+        name: 'Product',
+        image: require('../../shared/images/shirt/shirt4.jpg').default,
+        category: 'clothes',
+        new: false,
+        ranking: 'last',
+        featured: false,
+        hot: false,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 19,
+        name: 'Product',
+        image: require('../../shared/images/shirt/shirt5.jpg').default,
+        category: 'clothes',
+        new: true,
+        ranking: 'last',
+        featured: true,
+        hot: true,
+        size: {
+            xs: 'XS',
+            s: 'S',
+            m: 'M',
+            l: 'L',
+            xl: 'XL',
+            xxl: 'XXL'
+        },
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 20,
+        name: 'Product',
+        image: require('../../shared/images/vegetables/vegetables1.jpg').default,
+        category: 'organic',
+        new: true,
+        ranking: 'top',
+        featured: false,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 21,
+        name: 'Product',
+        image: require('../../shared/images/vegetables/vegetables2.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'middle',
+        featured: true,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 22,
+        name: 'Product',
+        image: require('../../shared/images/vegetables/vegetables3.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'middle',
+        featured: false,
+        hot: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 23,
+        name: 'Product',
+        image: require('../../shared/images/vegetables/vegetables4.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'last',
+        featured: false,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 24,
+        name: 'Product',
+        image: require('../../shared/images/vegetables/vegetables5.jpg').default,
+        category: 'organic',
+        new: true,
+        ranking: 'middle',
+        featured: true,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 25,
+        name: 'Product',
+        image: require('../../shared/images/vegetables/vegetables6.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'last',
+        featured: false,
+        hot: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 26,
+        name: 'Product',
+        image: require('../../shared/images/vegetables/vegetables7.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'top',
+        featured: true,
+        hot: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 27,
+        name: 'Product',
+        image: require('../../shared/images/fruit/fruit1.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'last',
+        featured: true,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 28,
+        name: 'Product',
+        image: require('../../shared/images/fruit/fruit2.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'last',
+        featured: true,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 29,
+        name: 'Product',
+        image: require('../../shared/images/fruit/fruit3.jpg').default,
+        category: 'organic',
+        new: true,
+        ranking: 'middle',
+        featured: true,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 30,
+        name: 'Product',
+        image: require('../../shared/images/fruit/fruit4.jpg').default,
+        category: 'organic',
+        new: false,
+        ranking: 'top',
+        featured: false,
+        hot: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 31,
+        name: 'Product',
+        image: require('../../shared/images/fruit/fruit5.jpg').default,
+        category: 'organic',
+        new: true,
+        ranking: 'last',
+        featured: false,
+        hot: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
+    },
+    {
+        id: 32,
+        name: 'Product',
+        image: require('../../shared/images/fruit/fruit6.jpg').default,
+        category: 'organic',
+        new: true,
+        ranking: 'top',
+        featured: true,
+        hot: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco."
     }
 ];
